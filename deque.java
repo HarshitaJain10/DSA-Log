f565761131b954ad9b7dde866de744ca25feb658
HarshitaJain10 <91328954+HarshitaJain10@users.noreply.github.com>
2026-05-17 03:19:38 +0530

    Add Stack and Queue implementations
    Implemented Stack and Queue classes using Deque.

---
+++ deque.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class deque {                                        //STACK
+    static class Stack{
+        Deque<Integer> deque=new LinkedList<>();
+        public void push(int data){
+            deque.addLast(data);
+        }
+        public int pop(){
+            return deque.removeLast();
+        }
+        public int peek(){
+            return deque.getLast();
+        }
+    }
+    static class queue{
+        Deque<Integer> deque=new LinkedList<>();
+        public void add(int data){
+            deque.addLast(data);
+        }
+        public int remove(){
+            return deque.removeFirst();
+        }
+        public int peek(){
+            return deque.getFirst();
+        }
+
+    }
+    public static void main(String[] args) {
+        Queue q =new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        Stack s=new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        System.out.println(s.peek());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        
+        System.out.println(s.pop());
+        
+        
+    }
+
+}
