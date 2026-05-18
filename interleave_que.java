dd11a81536496217b3885effa7f27af38dde8b31
HarshitaJain10 <91328954+HarshitaJain10@users.noreply.github.com>
2026-05-18 23:19:59 +0530

    Add interleave method for queue elements
    Implemented a method to interleave the elements of a queue.

---
+++ interleave_que.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class interleave_que {                          //0(n)
+    public static void inter(Queue<Integer>q){
+        Queue<Integer> first=new LinkedList<>();
+    int size=q.size();
+    for(int i=0;i<size/2;i++){
+        first.add(q.remove());
+    }
+    while(!first.isEmpty()){
+        q.add(first.remove());
+        q.add(q.remove());
+    }
+
+    }
+    
+    public static void main(String[] args) {
+        Queue<Integer> q=new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        inter(q);
+        while(!q.isEmpty()){
+            System.out.println(q.remove());
+        }
+        
+    }
+
+}
